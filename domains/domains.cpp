2e336f0920c3dfa80a6d12384e2ee953b29f6731
michael-novikov <michael.alex.novikov@gmail.com>
2020-09-26 18:10:03 +0300

    domains - fix runtime problem with address sanitizer

--- domains/domains.cpp
+++ domains/domains.cpp
@@ -71,7 +71,7 @@ bool IsSubdomain(const Domain& subdomain, const Domain& domain) {
   return
       subdomain.GetPartCount() >= domain.GetPartCount()
       && equal(begin(domain_reversed_parts), end(domain_reversed_parts),
-               end(subdomain_reversed_parts));
+               begin(subdomain_reversed_parts));
 }
 
 bool IsSubOrSuperDomain(const Domain& lhs, const Domain& rhs) {
@@ -85,7 +85,7 @@ class DomainChecker {
 public:
   template <typename InputIt>
   DomainChecker(InputIt domains_begin, InputIt domains_end) {
-    sorted_domains_.resize(distance(domains_begin, domains_end));
+    sorted_domains_.reserve(distance(domains_begin, domains_end));
     for (const Domain& domain : Range(domains_begin, domains_end)) {
       sorted_domains_.push_back(&domain);
     }
@@ -101,7 +101,7 @@ public:
     if (it == begin(sorted_domains_)) {
       return false;
     }
-    return ::IsSubdomain(candidate, **it);
+    return ::IsSubdomain(candidate, **prev(it));
   }
 
 private:
