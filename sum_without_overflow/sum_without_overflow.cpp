630f6cf9d966d40617e77934dec156f9ac4c5cda
michael-novikov <michael.alex.novikov@gmail.com>
2020-10-10 15:13:28 +0300

    sum_without_overflow - handle minimal int case

--- sum_without_overflow/sum_without_overflow.cpp
+++ sum_without_overflow/sum_without_overflow.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-bool SumOverflow(int64_t lhs, int64_t rhs) {
-    auto lhs_u = static_cast<uint64_t>(lhs < 0 ? -lhs : lhs);
-    auto rhs_u = static_cast<uint64_t>(rhs < 0 ? -rhs : rhs);
+bool IntegerSumOverflow(double lhs, double rhs) {
+    auto lhs_u = lhs < 0 ? -lhs : lhs;
+    auto rhs_u = rhs < 0 ? -rhs : rhs;
     auto sum_u = lhs_u + rhs_u;
 
-    return sum_u > static_cast<uint64_t>(INT64_MAX);
+    return sum_u > static_cast<double>(INT64_MAX);
 }
 
 int main() {
@@ -16,7 +16,7 @@ int main() {
 
     cin >> lhs >> rhs;
 
-    if (SumOverflow(lhs, rhs)) {
+    if (IntegerSumOverflow(lhs, rhs)) {
         cout << "Overflow!" << endl;
         return -1;
     }
