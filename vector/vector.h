67379f45e6408df57f191e24083b525fe4154d97
michael-novikov <michael.alex.novikov@gmail.com>
2021-02-26 20:34:03 +0200

    vector - add raw memory wrapping class

--- vector/vector.h
+++ vector/vector.h
@@ -1,4 +1,68 @@
 #include <cstddef>
+#include <utility>
+
+template <typename T>
+class RawMemory {
+public:
+  RawMemory() = default;
+  RawMemory(size_t capacity);
+  ~RawMemory();
+
+  const T& operator[](size_t idx) const;
+  T& operator[](size_t idx);
+
+  const T* operator+(size_t idx) const;
+  T* operator+(size_t idx);
+private:
+  T* data_ = nullptr;
+  size_t capacity_;
+
+  static T* Allocate(size_t count);
+  static void Free(T* data);
+};
+
+template <typename T>
+static T* Allocate(size_t count) {
+  return static_cast<T*>(operator new(count * sizeof(T)));
+}
+
+template <typename T>
+static void Free(T* data) {
+  operator delete(data);
+}
+
+template <typename T>
+RawMemory<T>::RawMemory(size_t capacity)
+{
+  data_ = Allocate(capacity);
+  capacity_ = capacity;
+}
+
+template <typename T>
+RawMemory<T>::~RawMemory()
+{
+  Free(data_);
+}
+
+template <typename T>
+const T& RawMemory<T>::operator[](size_t idx) const {
+  return data_[idx];
+}
+
+template <typename T>
+T& RawMemory<T>::operator[](size_t idx) {
+  return data_[idx];
+}
+
+template <typename T>
+const T* RawMemory<T>::operator+(size_t idx) const {
+  return data_ + idx;
+}
+
+template <typename T>
+T* RawMemory<T>::operator+(size_t idx) {
+  return data_ + idx;
+}
 
 template <typename T>
 class Vector {
